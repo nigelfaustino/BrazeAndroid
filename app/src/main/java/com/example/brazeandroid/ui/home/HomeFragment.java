922433bdd4640a4f87282e8b871ffb3126f577fb
Nigel Faustino <nigelfaustino@gmail.com>
2021-04-13 09:22:20 -0700

    Log custom event

--- app/src/main/java/com/example/brazeandroid/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/brazeandroid/ui/home/HomeFragment.java
@@ -4,14 +4,11 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
-import android.view.View;
+
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
@@ -20,13 +17,14 @@ import com.appboy.support.StringUtils;
 import com.example.brazeandroid.R;
 
 import com.appboy.Appboy;
-import com.appboy.AppboyUser;
 
 public class HomeFragment extends Fragment {
 
     private HomeViewModel homeViewModel;
     private Button changeUserButton;
     private EditText mUserIdEditText;
+    private Button logCustomEventButton;
+    private EditText customEventText;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -34,7 +32,9 @@ public class HomeFragment extends Fragment {
                 ViewModelProviders.of(this).get(HomeViewModel.class);
         View root = inflater.inflate(R.layout.fragment_home, container, false);
         changeUserButton = root.findViewById(R.id.button);
+        logCustomEventButton = root.findViewById(R.id.eventButton);
         mUserIdEditText = root.findViewById(R.id.editText);
+        customEventText = root.findViewById(R.id.attributeText);
         return root;
     }
 
@@ -48,6 +48,15 @@ public class HomeFragment extends Fragment {
                 Toast.makeText(getContext(), "Set userId to: " + userId, Toast.LENGTH_SHORT).show();
             } else {
                 Toast.makeText(getContext(), "Please enter a userId.", Toast.LENGTH_SHORT).show();
+            }
+        });
+        logCustomEventButton.setOnClickListener(view -> {
+            String event = customEventText.getText().toString();
+            if (!StringUtils.isNullOrBlank(event)) {
+                Appboy.getInstance(getContext()).logCustomEvent(event);
+                Toast.makeText(getContext(), "Logging custom event: " + event, Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(getContext(), "Please enter a custom event.", Toast.LENGTH_SHORT).show();
             }
         });
     }
